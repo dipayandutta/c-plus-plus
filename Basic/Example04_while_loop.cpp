37206d76eb9b94f61ba2d540375534027aa0fed8
Dipayan Dutta <inbox.dipayan@gmail.com>
2018-08-22 20:16:53 +0530

    A simple for loop Example

---
+++ Basic/Example04_while_loop.cpp
@@ -0,0 +1,20 @@
+/*A simple while loop Example*/
+
+#include <iostream>
+
+using std::cout ;
+using std::endl;
+
+/*printing 0..9 on the screen*/
+int main(){
+	
+		int i=0;
+
+		while(i<10){
+			cout << i<<"th Element is => "<<i;
+			cout << endl;
+			i++;
+		}
+
+		return 0;
+	}
