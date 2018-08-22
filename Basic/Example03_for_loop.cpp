fffdc1fcceaa8ae971bbbba91168496e2b60d446
Dipayan Dutta <inbox.dipayan@gmail.com>
2018-08-22 20:12:37 +0530

    A Basic for loop

---
+++ Basic/Example03_for_loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+/*A simple for loop example*/
+/*Print 0..9 numbers */
+int main(){
+	
+		int i = 0;
+		for(i=0;i<10;i++){
+				cout << i <<"th Element is => "<< i;
+				cout << endl;
+			}
+
+		return 0;
+	}
