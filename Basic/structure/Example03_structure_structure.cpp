a7399e120519e190fd43a8eb0fa6a4b62267e0c5
Dipayan Dutta <inbox.dipayan@gmail.com>
2018-08-30 09:18:57 +0530

    structure example

--- Basic/structure/Example03_structure_structure.cpp
+++ Basic/structure/Example03_structure_structure.cpp
@@ -12,3 +12,22 @@ struct Room {
     Distance length;
     Distance width;
 };
+
+int main(){
+    
+    Room dining;
+    dining.length.feet = 10;
+    dining.width.inches = 10.10;
+    
+    float result = dining.length.feet + dining.width.inches;
+    cout << result;
+    cout << endl;
+    
+    cout << "Press Enter to exit";
+    cout << endl;
+    
+    cin.ignore();
+    cin.ignore();
+    
+    return 0;
+}
