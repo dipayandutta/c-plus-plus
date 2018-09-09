12fe60286b8ac9615e732336ebf6663009bf2ed2
Dipayan Dutta <inbox.dipayan@gmail.com>
2018-09-09 23:10:00 +0530

    Multiple files

---
+++ intermediate/multifile/test/add_header.h
@@ -0,0 +1,7 @@
+#ifndef ADD_HEADER_H_INCLUDED
+#define ADD_HEADER_H_INCLUDED
+
+
+
+#endif // ADD_HEADER_H_INCLUDED
+int add(int x,int y);
---
+++ intermediate/multifile/test/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "add_header.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    int result = add(10,20);
+    cout << endl;
+    cout << result;
+    return 0;
+
+}
