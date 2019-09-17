b2b681d2e236c1f118e8f1a72e24a755148f00b5
Dipayan Dutta <inbox.dipayan@gmail.com>
2019-09-17 15:04:53 +0530

    Template Function Example

--- templates/simpleFunctionTemplate.cpp
+++ templates/simpleFunctionTemplate.cpp
@@ -6,27 +6,27 @@ using namespace std;
 template <typename T> /*T is a name representating a type to be supplied later*/
 
 /*The inline functions are a C++ enhancement feature to increase the execution time of a program.*/
-inline T const& Max(T const& a, T const& b){
-	return a < b ? b:a;
+inline T addition(T  a, T  b){
+	return a +b ;
 }
 
 int main(void){
 	int i = 10;
 	int j = 20;
 
-	cout << "MAX(i,j)"<< Max(i,j);
+	cout << "addition(i,j)"<< addition(i,j);
 	cout << endl;
 
 	double k = 10.21;
 	double l = 11.00;
 
-	cout << "MAC(k,l)"<< Max(k,l);
+	cout << "addition(k,l)"<< addition(k,l);
 	cout << endl;
 
 	string s1 = "Hello";
 	string s2 = "World";
 
-	cout << "MAX(s1,s2)"<< Max(s1,s1);
+	cout << "addition(s1,s2)"<< addition(s1,s1);
 	cout << endl;
 
 	return 0;
